0ac296c5908d72880dc32d83d4d5bd2af49663c4
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-29 13:40:28 +0100

    Added welcome page tables

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,11 @@ class Review < ApplicationRecord
   belongs_to :activity
 
   def self.sort_highest
-    ratings = self.all.sort_by{|review| review.rating}.reverse
-    ratings[0 ... 10]
+    self.all.sort_by{|review| review.rating}.reverse[0 ... 10]
+  end
+
+  def self.top_countries
+    tc = TripCountry.all.map{|a| a.country_id}.sort
+    tc.inject(Hash.new(0)) { |country_id, count| country_id[count] += 1; country_id}.max_by(3){|country_id,count| count; count}.map { |arr| arr[0]}
   end
 end
