8ded29999895ed19386e0ac9e07d427b66b062e9
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-29 09:30:24 +0100

    Remerged conflicts

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :activity
+
+  def self.sort_highest
+    ratings = self.all.sort_by{|review| review.rating}.reverse
+    ratings[0 ... 10]
+  end
 end
